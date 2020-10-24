f994365c296266ad3577988af4787ccfe7195ff7
s1710931010 <73382566+s1710931010@users.noreply.github.com>
2020-10-24 17:00:44 +0800

    Update configure-iis.ps1

--- configure-iis.ps1
+++ configure-iis.ps1
@@ -4,4 +4,4 @@ dism /online /enable-feature /featurename:IIS-WebServerRole
 # Set the home page.
 Set-Content `
   -Path "C:\\inetpub\\wwwroot\\Default.htm" `
-  -Value "<html><body><h2>Welcome to Azure! My name is kenny 20201024 $($env:computername).</h2></body></html>"
+  -Value "<html><body><h2>Welcome to Azure! My name is Max 20201024 $($env:computername).</h2></body></html>"
