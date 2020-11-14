0f84ea4fa1a1f0caea640e44a83c1812205329a6
s1710931010 <73382566+s1710931010@users.noreply.github.com>
2020-11-14 11:57:38 +0800

    Update configure-iis.ps1

--- configure-iis.ps1
+++ configure-iis.ps1
@@ -4,4 +4,4 @@ dism /online /enable-feature /featurename:IIS-WebServerRole
 # Set the home page.
 Set-Content `
   -Path "C:\\inetpub\\wwwroot\\Default.htm" `
-  -Value '<html><head><meta charset="UTF-8" /></head><body><h2>1710931010 張智湧 期中作業</h2><hr><iframe width="1024" height="612" src="https://app.powerbi.com/view?r=eyJrIjoiYzZiYzE0NmUtMjUyNS00MTJhLWJkZDktZmFmZTM1ZWI1OTY2IiwidCI6IjI4ZDBmYTc1LWY5ZjktNDAyNC05MzM3LTQ4NWQ0NmU3NTI1NyIsImMiOjEwfQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe></body></html>'
+  -Value '<html><head><meta charset="UTF-8" /></head><body><h2>1710931010 張智湧 期中作業</h2><hr><iframe width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiMzVlMTBhYTctZjA0Ni00YTliLThlZWUtNWQxYjE4MjYyODg2IiwidCI6IjI4ZDBmYTc1LWY5ZjktNDAyNC05MzM3LTQ4NWQ0NmU3NTI1NyIsImMiOjEwfQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe></body></html>'
